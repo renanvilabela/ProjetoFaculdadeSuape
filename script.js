f8a95960609f6457c8d950207c33a1f1a50387bd
renanvilabela <vilabelarenan@gmail.com>
2023-09-25 16:18:43 -0300

    [FEAT] criação de variáveis

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("dale")
+
+var notification = document.querySelector("#notification");
+var open = document.querySelector("#open");
+var emergency = document.querySelector("#emergency"); 
