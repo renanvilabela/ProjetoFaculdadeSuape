b5040b1c8e24c5f37bfad88305cf3d323cdf6539
renanvilabela <vilabelarenan@gmail.com>
2023-09-26 13:32:36 -0300

    [FEAT] setando as funções

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ console.log("dale")
 var notification = document.querySelector("#notification");
 var open = document.querySelector("#open");
 var emergency = document.querySelector("#emergency"); 
+
+function AbrirChamado(){
+    section.innerHTML = `
+
+    `
+}
+
+function Emergencial(){
+
+}
+
+function notification(){
+    
+}
