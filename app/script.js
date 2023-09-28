c7a1e0b93254f0fabb9d4ee78cb74c0d691a569f
renanvilabela <vilabelarenan@gmail.com>
2023-09-28 17:04:35 -0300

    [FEAT] remodelação das variáveis globais, remodelação da function sendForm, criação da função Yourcalls, para mostrar os dados pegos pela função send form ao usuário

--- app/script.js
+++ app/script.js
@@ -1,9 +1,9 @@
 console.log("dale")
 
-var notification = document.querySelector("#notification");
-var open = document.querySelector("#open");
-var emergency = document.querySelector("#emergency"); 
 let origin = null //variável para armazenar o conteúdo original da página
+let identificador = '' //variavel vazia
+let problema = '' //variavel vazia
+let tipos = [] //array vazia
 
 //função que abre o formulário
 function AbrirChamado(){
@@ -37,18 +37,35 @@ function sendForm(){
     //use o método FormData para coletar os dados do formulário
     const formData = new FormData(formulario);
     //você pode acessar os valores do formData usando get() ou getALL() para campos de múltiplos valores (como checkboxes)
-    const identificador = formData.get('identificador')
-    const problema = formData.get('problema');
-    const tipos = formData.getAll('tipo')
+    identificador = formData.get('identificador')
+    problema = formData.get('problema');
+    tipos = formData.getAll('tipo')
 
     console.log(`Identificador: ${identificador}\nProblema: ${problema}\nTipos: ${tipos.join(', ')}`);
 }
 
 function YourCalls(){
-    window.alert("seus chamados")
+    const calls = document.querySelector('#chamados');
+    //crie o conteúdo html com os dados dos chamados
+    if (identificador && problema && tipos.length > 0){
+        const conteudoHTML = `
+            <h2>Seus Chamados</h2>
+            <p id="registro">Identificador: ${identificador}</p>
+            <p id="problema-registrado">Problema: ${problema}</p>
+            <p id="check">tipos: ${tipos.join(', ')}</p> 
+        `;
+        //insira o conteúdo na "caixa" para exibir os chamados
+        calls.innerHTML = conteudoHTML;
+        //mostra a caixa definindo o estilo display para "block"
+        calls.style.display = 'flex';
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    YourCalls(); //chama a função após o carregamento da página
+});
     
 
-function notificação(){
-    console.log("olha o zap")
-}
+// function notificação(){
+//     console.log("olha o zap")
+// }
