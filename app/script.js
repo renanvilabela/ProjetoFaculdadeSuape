627a81c285982aebb05f9cc9e6c864e19050b34e
renanvilabela <vilabelarenan@gmail.com>
2023-09-28 14:41:14 -0300

    [FEAT] implementação de função para retornar o conteúdo original da página

--- app/script.js
+++ app/script.js
@@ -3,19 +3,33 @@ console.log("dale")
 var notification = document.querySelector("#notification");
 var open = document.querySelector("#open");
 var emergency = document.querySelector("#emergency"); 
+let origin = null //variável para armazenar o conteúdo original da página
 
+//função que abre o formulário
 function AbrirChamado(){
     window.alert("abre chamado")
-    //arquivo que será lido ↓
+    //verifica se o conteúdo original ainda não foi armazenado
+    if(!origin){
+        origin = document.querySelector('main').innerHTML; //armazena conteúdo original
+    }
+    //arquivo do form que será lido ↓
     fetch('form/form.html') 
         .then(response => response.text())
         .then(data => {
             const main = document.querySelector('main');
-            main.innerHTML = data;
+            main.innerHTML = data; //substitui o conteúdo da <main> pelo conteúdo do formulário
         })
         .catch(error => {
             console.error('Erro ao carregar o formulário', error);
         })
+}
+
+//função para voltar ao conteúdo original
+function voltar(){
+    if (origin){
+        const main = document.querySelector('main');
+        main.innerHTML = origin; //restaura o conteúdo original da <main>
+    }
 }
 
 function YourCalls(){
