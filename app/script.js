9ac2a72f668a949bda78dc4da800e83914105a58
renanvilabela <vilabelarenan@gmail.com>
2023-10-04 14:39:14 -0300

    teste de funcionalidades

--- app/script.js
+++ app/script.js
@@ -4,6 +4,7 @@ let origin = null //variável para armazenar o conteúdo original da página
 let identificador = '' //variavel vazia
 let problema = '' //variavel vazia
 let tipos = [] //array vazia
+var target = document.getElementById('target');
 
 //função que abre o formulário
 function AbrirChamado(){
@@ -63,7 +64,74 @@ function YourCalls(){
 document.addEventListener('DOMContentLoaded', function(){
     YourCalls(); //chama a função após o carregamento da página
 });
+
+
+//localização
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
     
+}
+
+// function getUserMedia(constraints) {
+//     // if Promise-based API is available, use it
+//     if (navigator.mediaDevices) {
+//       return navigator.mediaDevices.getUserMedia(constraints);
+//     }
+      
+//     // otherwise try falling back to old, possibly prefixed API...
+//     var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+//       navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      
+//     if (legacyApi) {
+//       // ...and promisify it
+//       return new Promise(function (resolve, reject) {
+//         legacyApi.bind(navigator)(constraints, resolve, reject);
+//       });
+//     }
+//   }
+  
+//   function getStream (type) {
+//     if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+//       !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+//       alert('User Media API not supported.');
+//       return;
+//     }
+  
+//     var constraints = {};
+//     constraints[type] = true;
+    
+//     getUserMedia(constraints)
+//       .then(function (stream) {
+//         var mediaControl = document.querySelector(type);
+        
+//         if ('srcObject' in mediaControl) {
+//           mediaControl.srcObject = stream;
+//         } else if (navigator.mozGetUserMedia) {
+//           mediaControl.mozSrcObject = stream;
+//         } else {
+//           mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+//         }
+        
+//         mediaControl.play();
+//       })
+//       .catch(function (err) {
+//         alert('Error: ' + err);
+//       });
+//   }
 
 // function notificação(){
 //     console.log("olha o zap")
