8af409c3a85272146c8a580d4b52ea2161bf0cbb
renanvilabela <vilabelarenan@gmail.com>
2023-10-05 15:36:12 -0300

    [FEAT] inclusão de nova function

--- app/script.js
+++ app/script.js
@@ -6,6 +6,24 @@ const identificador = localStorage.getItem('identificador') || '';
 let tipos = [] //array vazia
 var target = document.getElementById('target');
 
+
+function fazerLogin() {
+    // Obter o identificador do usuário a partir do campo de login
+    const identificador = document.getElementById('log-id').value;
+  
+    // Ocultar a seção de login
+    document.querySelector('.login-area').style.display = 'none';
+  
+    // Exibir a seção de conteúdo principal
+    document.querySelector('main').style.display = 'flex';
+  
+    // Armazenar o identificador em localStorage para uso posterior
+    localStorage.setItem('identificador', identificador);
+  
+    // Registre o identificador no console (opcional)
+    console.log(`Identificador: ${identificador}`);
+  }
+
 //função que abre o formulário
 function AbrirChamado(){
     //verifica se o conteúdo original ainda não foi armazenado
@@ -71,75 +89,3 @@ function YourCalls(){
 document.addEventListener('DOMContentLoaded', function(){
     YourCalls(); //chama a função após o carregamento da página
 });
-
-
-//localização
-// function appendLocation(location, verb) {
-//   verb = verb || 'updated';
-//   var newLocation = document.createElement('p');
-//   newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
-//   target.appendChild(newLocation);
-// }
-
-// if ('geolocation' in navigator) {
-//   document.getElementById('askButton').addEventListener('click', function () {
-//     navigator.geolocation.getCurrentPosition(function (location) {
-//       appendLocation(location, 'fetched');
-//     });
-//     watchId = navigator.geolocation.watchPosition(appendLocation);
-//   });
-// } else {
-//   target.innerText = 'Geolocation API not supported.';
-    
-// }
-
-// function getUserMedia(constraints) {
-//     // if Promise-based API is available, use it
-//     if (navigator.mediaDevices) {
-//       return navigator.mediaDevices.getUserMedia(constraints);
-//     }
-      
-//     // otherwise try falling back to old, possibly prefixed API...
-//     var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-//       navigator.mozGetUserMedia || navigator.msGetUserMedia;
-      
-//     if (legacyApi) {
-//       // ...and promisify it
-//       return new Promise(function (resolve, reject) {
-//         legacyApi.bind(navigator)(constraints, resolve, reject);
-//       });
-//     }
-//   }
-  
-//   function getStream (type) {
-//     if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-//       !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-//       alert('User Media API not supported.');
-//       return;
-//     }
-  
-//     var constraints = {};
-//     constraints[type] = true;
-    
-//     getUserMedia(constraints)
-//       .then(function (stream) {
-//         var mediaControl = document.querySelector(type);
-        
-//         if ('srcObject' in mediaControl) {
-//           mediaControl.srcObject = stream;
-//         } else if (navigator.mozGetUserMedia) {
-//           mediaControl.mozSrcObject = stream;
-//         } else {
-//           mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-//         }
-        
-//         mediaControl.play();
-//       })
-//       .catch(function (err) {
-//         alert('Error: ' + err);
-//       });
-//   }
-
-// function notificação(){
-//     console.log("olha o zap")
-// }
