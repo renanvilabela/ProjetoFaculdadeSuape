629e0c1511d15b961abfcbcfae3b2d372c521572
renanvilabela <vilabelarenan@gmail.com>
2023-12-11 18:12:22 -0300

    [FEATURE] conexão com o anco de dados

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ const mongoose = require('mongoose');
 
 const app = express()
 const port = 3043
+mongoose.connect('mongodb+srv://vilabelarenan:XJI7RWu8gaLPX6ur@securitysuape.wpwnbix.mongodb.net/?retryWrites=true&w=majority');
+
+const data = mongoose.model('teste',{
+    title: String,
+    description: String,
+    image_url: String,
+    url: String,
+});
 
 app.get("/", (req, res)=> {
-    res.send(`<h1>dale boy</h1>`)
+    res.send('app funcionando')
+});
+
+app.post("/", async (req, res) => {
+    const data = new data({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        url: req.body.url
+    })
+
+    await data.save
+    res.send(data)
 })
 
 app.listen(port, () => {
